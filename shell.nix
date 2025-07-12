1cfea69fa457432d795486b7bd1e537028cd7939
owuh <owuh@wayland.best>
2025-07-11 21:42:33 -0500

    make shell.nix read from requirements.txt instead of hardcoding

--- shell.nix
+++ shell.nix
@@ -1,12 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-	buildInputs = [
-		(pkgs.python3.withPackages (ps: with ps; [
-			discordpy
-			httpx
-			openai
-			pyyaml
-		]))
-	];
+  buildInputs = [
+    pkgs.python3
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.virtualenv
+  ];
+
+  shellHook = ''
+    if [ ! -d .venv ]; then
+      python -m venv .venv
+      . .venv/bin/activate
+      pip install -r requirements.txt
+    else
+      . .venv/bin/activate
+    fi
+  '';
 }
